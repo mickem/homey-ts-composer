a711dde97e0b3e7a242d41550b9d9162289c1136
Michael Medin <michael.medin@pricer.com>
2019-12-17 20:57:29 +0100

    1.0.1

---
+++ src/__tests__/TriggerParser.test.ts
@@ -0,0 +1,170 @@
+import { processTriggers } from "../TriggerParser";
+import { parseCode } from "../Utils";
+
+describe("full example", () => {
+  const code = `
+    export interface ITriggers {
+        /**
+         * A temperature changed
+         * @param args.zone zone #sample:Kitchen
+         * @param args.temperature average temperature #sample:14.5
+         */
+        TemperatureChanged(args: { zone: string, temperature: number }): Promise<void>;
+        /**
+         * The maximum temperature for a zone changed
+         * @param args.zone zone #sample:Kitchen
+         * @param args.device Thermometer #sample:Wall thermometer
+         * @param args.temperature maximum temperature #sample:14.5
+         */
+        MaxTemperatureChanged(args: { zone: string, device: string, temperature: number }): Promise<void>;
+        /**
+         * The minimum temperature for a zone changed
+         * @param args.zone zone #sample:Kitchen
+         * @param args.device Thermometer #sample:Wall thermometer
+         * @param args.temperature minimum temperature #sample:14.5
+         */
+        MinTemperatureChanged(args: { zone: string, device: string, temperature: number }): Promise<void>;
+        /**
+         * The temperature is too warm
+         * @param args.zone zone #sample:Kitchen
+         * @param args.temperature temperature #sample:14.5
+         */
+        TooWarm(args: { zone: string, temperature: number }): Promise<void>;
+        /**
+         * The temperature is too cold
+         * @param args.zone zone #sample:Kitchen
+         * @param args.temperature temperature #sample:14.5
+         */
+        TooCold(args: { zone: string, temperature: number }): Promise<void>;
+      }
+      
+    `;
+  test("should parse correctly", () => {
+    expect(processTriggers(parseCode(code))).toEqual([
+      {
+        id: "TemperatureChanged",
+        title: {
+          en: "A temperature changed"
+        },
+        tokens: [
+          {
+            name: "zone",
+            title: {
+              en: "zone"
+            },
+            type: "string"
+          },
+          {
+            name: "temperature",
+            title: {
+              en: "average temperature"
+            },
+            type: "number"
+          }
+        ]
+      },
+      {
+        id: "MaxTemperatureChanged",
+        title: {
+          en: "The maximum temperature for a zone changed"
+        },
+        tokens: [
+          {
+            name: "zone",
+            title: {
+              en: "zone"
+            },
+            type: "string"
+          },
+          {
+            name: "device",
+            title: {
+              en: "Thermometer"
+            },
+            type: "string"
+          },
+          {
+            name: "temperature",
+            title: {
+              en: "maximum temperature"
+            },
+            type: "number"
+          }
+        ]
+      },
+      {
+        id: "MinTemperatureChanged",
+        title: {
+          en: "The minimum temperature for a zone changed"
+        },
+        tokens: [
+          {
+            name: "zone",
+            title: {
+              en: "zone"
+            },
+            type: "string"
+          },
+          {
+            name: "device",
+            title: {
+              en: "Thermometer"
+            },
+            type: "string"
+          },
+          {
+            name: "temperature",
+            title: {
+              en: "minimum temperature"
+            },
+            type: "number"
+          }
+        ]
+      },
+      {
+        id: "TooWarm",
+        title: {
+          en: "The temperature is too warm"
+        },
+        tokens: [
+          {
+            name: "zone",
+            title: {
+              en: "zone"
+            },
+            type: "string"
+          },
+          {
+            name: "temperature",
+            title: {
+              en: "temperature"
+            },
+            type: "number"
+          }
+        ]
+      },
+      {
+        id: "TooCold",
+        title: {
+          en: "The temperature is too cold"
+        },
+        tokens: [
+          {
+            name: "zone",
+            title: {
+              en: "zone"
+            },
+            type: "string"
+          },
+          {
+            name: "temperature",
+            title: {
+              en: "temperature"
+            },
+            type: "number"
+          }
+        ]
+      }
+    ]);
+  });
+});
